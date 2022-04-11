cb44507a6da6b1675e8b87538db245fd58c579f6
Tristan Rhoan <trhoan@fetchrobotics.com>
2022-04-11 15:57:03 -0700

    Updated script

--- slap_house_chores.py
+++ slap_house_chores.py
@@ -1,106 +1,70 @@
 from collections import deque
 import smtplib
 from email.message import EmailMessage
-from twilio.rest import Client
 
-fname = "/Users/mc98090/desktop/file1.txt"
+fname = "choreFile.txt"
 
 # opening the file and read contents
 my_file = open(fname, "r")
-  
-# reading the file
+
+# reading the file and removing any empty strings
 data = my_file.read()
 data_into_list = data.split("\n")
-print("Initial list:")
-print(data_into_list)
+data_into_list = ' '.join(data_into_list).split()
+print(f'[INFO] Initial list: {data_into_list}')
 my_file.close()
 
 # Naming and contact list for housemates
-slapHouseResidents= {"Ash": "", "Keyan": "", "Melissa": "", "Brian": "", "Kevin": "", "Tristan":""}
-chores = data_into_list
-emails={"ash":"7163482003@tmomail.net", "melissa":"3238235882@vtext.com", "tristan":"5597601868@vtext.com", "brian": "9185207346@txt.att.net" , "keyan": "9259899386@vtext.com", "kevin":"8583665910@vtext.com" }
-k = 1
-j =0
-x = deque(chores)
-x.rotate(k)
-print("Post rotate list:")
-print(x) 
-chores = list(x)
+slapHouseResidents = {"Ash": "",
+                      "Keyan": "",
+                      "Melissa": "",
+                      "Brian": "",
+                      "Kevin": "",
+                      "Tristan": ""
+                      }
+emails = {"Ash": "7163482003@tmomail.net",
+          "Melissa": "3238235882@vtext.com",
+          "Tristan": "5597601868@vtext.com",
+          "Brian": "9185207346@txt.att.net",
+          "Keyan": "9259899386@vtext.com",
+          "Kevin": "8583665910@vtext.com"
+          }
 
-#Assign all chores
-for i in slapHouseResidents:
-	slapHouseResidents[i]= x[j]
-	j+=1
-print("Final chore assignments:")
-print(slapHouseResidents)
+chores = deque(data_into_list)
+chores.rotate(1)
+print(f"[INFO] Post rotate list: {chores}")
 
-#Write new list to file
+# Assign all chores
+j = 0
+prettyList = simpleList = ""
+for resident in list(slapHouseResidents):
+    slapHouseResidents[resident] = chores[j]
+    prettyList += f"\n{resident}: {slapHouseResidents[resident]}"
+    simpleList += f"{resident}: {slapHouseResidents[resident]} "
+    j += 1
+print(f"[INFO] Final chore assignments:{prettyList}")
+
+# Write new list to file
 with open(fname, 'w') as f:
-    for item in x:
-        f.write("%s\n" % item)
+    for chore in chores:
+        f.write(f'{chore}\n')
 
-# Remove any empty rows
-with open(fname) as f_input:
-    data = f_input.read().rstrip('\n')
-
-with open(fname, 'w') as f_output:    
-    f_output.write(data)
-
-
-# Sending...
-
-msg = EmailMessage()
-msg.set_content('Hi Ash, your chores for the week: ' + slapHouseResidents["Ash"])
-
-msg['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg['From'] = "slaphouse4.0@gmail.com"
-msg['To'] = emails["ash"]
-
-msg2 = EmailMessage()
-msg2.set_content('Hi Melissa, your chore for the week: ' + slapHouseResidents["Melissa"])
-
-msg2['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg2['From'] = "slaphouse4.0@gmail.com"
-msg2['To'] = emails["melissa"]
-
-msg3 = EmailMessage()
-msg3.set_content('Hi Tristan, your chore for the week: ' + slapHouseResidents["Tristan"])
-
-msg3['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg3['From'] = "slaphouse4.0@gmail.com"
-msg3['To'] = emails["tristan"]
-
-msg4 = EmailMessage()
-msg4.set_content('Hi Brian, your chores for the week: ' + slapHouseResidents["Brian"])
-
-msg4['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg4['From'] = "slaphouse4.0@gmail.com"
-msg4['To'] = emails["brian"]
-
-msg5 = EmailMessage()
-msg5.set_content('Hi Keyan, your chores for the week: ' + slapHouseResidents["Keyan"])
-
-msg5['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg5['From'] = "slaphouse4.0@gmail.com"
-msg5['To'] = emails["keyan"]
-
-msg6 = EmailMessage()
-msg6.set_content('Hi Kevin, your chores for the week: ' + slapHouseResidents["Kevin"])
-
-msg6['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
-msg6['From'] = "slaphouse4.0@gmail.com"
-msg6['To'] = emails["kevin"]
-
-
-server  = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+# Login and sending...
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 server.login("slaphouse4.0@gmail.com", "Achintya_716")
-server.sendmail("achintya.pillai88@gmail.com", "achintya.pillai88@gmail.com", "Test Slaphouse")
-server.send_message(msg)
-server.send_message(msg2)
-server.send_message(msg3)
-server.send_message(msg4)
-server.send_message(msg5)
-server.send_message(msg6)
-server.send_message(msg2)
-server.quit()
+#server.sendmail("achintya.pillai88@gmail.com", "achintya.pillai88@gmail.com", "Test Slaphouse")
 
+for housemate in list(slapHouseResidents):
+    if housemate == "Tristan":
+        msg = EmailMessage()
+        content = f'Hi {housemate}, this week\'s chore list:{prettyList}\n'
+
+        # Set email message content and send to housemate
+        msg.set_content(content)
+        msg['Subject'] = 'SLAP HOUSE 4.0 CHORES ALERT'
+        msg['From'] = "slaphouse4.0@gmail.com"
+        msg['To'] = emails[housemate]
+
+        server.send_message(msg)
+
+server.quit()
