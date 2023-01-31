aeaf877e3762c4b8fcde87e55034ea54973cb1bf
Tristan Rhoan <trhoan@fetchrobotics.com>
2023-01-31 13:49:40 -0800

    Last edits

--- slap_house_chores.py
+++ slap_house_chores.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
+
 from collections import deque
 import smtplib
 from email.message import EmailMessage
 import argparse
 
-def send_chores(filename, remind=False):
+import re
+import asyncio
+import aiosmtplib
+
+HOST = "smtp.gmail.com"
+
+
+async def send_chores(filename, remind=False):
     """
     Reads current chore file in directory and rotates chores.
     This will then send the list of chores for all housemates to each housemate via phone smtp.
@@ -57,35 +66,44 @@ def send_chores(filename, remind=False):
             f.write(f'{chore}\n')
 
     # Login and sending...
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("slaphouse4.0@gmail.com", "Achintya_716")
-    #server.sendmail("achintya.pillai88@gmail.com", "achintya.pillai88@gmail.com", "Test Slaphouse")
+    # server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    # server.login("slaphouse4.0@gmail.com", "Achintya_716")
+    # server.sendmail("achintya.pillai88@gmail.com", "achintya.pillai88@gmail.com", "Test Slaphouse")
+
+    # GMail settings
 
     for housemate in list(slapHouseResidents):
         msg = EmailMessage()
         msg2 = EmailMessage()
 
         # Set email message content and send to housemate
-        content = f'Hi {housemate}, your chore for the week: {slapHouseResidents[housemate]}'
+        # content = f'Hi {housemate}, your chore for the week: {slapHouseResidents[housemate]}'
+        content = f'Hi {housemate}, this is SlapHouse chores signing off forever. Thanks for keeping Slaphouse clean. \n XOXO'
         msg.set_content(content)
         msg['Subject'] = 'SLAP HOUSE CHORES ALERT'
         msg['From'] = "slaphouse4.0@gmail.com"
         msg['To'] = emails[housemate]
 
-        server.send_message(msg)
+        # send
+        send_kws = dict(username="slaphouse4.0@gmail.com", password="bmjjxpzkmhnziyht", hostname=HOST, port=587, start_tls=True)
+        res = await aiosmtplib.send(msg, **send_kws)  # type: ignore
+        msg = "failed" if not re.search(r"\sOK\s", res[1]) else "succeeded"
+        print(msg)
 
-        # Set message content to full chore list for second text message
-        msg2_content = f'**** Chore List **** {prettyList}'
-        msg2.set_content(msg2_content)
-        msg2['From'] = "slaphouse4.0@gmail.com"
-        msg2['To'] = emails[housemate]
-        server.send_message(msg2)
+            # server.send_message(msg)
 
-    server.quit()
+        # # Set message content to full chore list for second text message
+        # msg2_content = f'**** Chore List **** {prettyList}'
+        # msg2.set_content(msg2_content)
+        # msg2['From'] = "slaphouse4.0@gmail.com"
+        # msg2['To'] = emails[housemate]
+        # server.send_message(msg2)
+
+    # server.quit()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Housemate Chore Rotator that will senda text to phone via smtp.')
+    parser = argparse.ArgumentParser(description='Housemate Chore Rotator that will send a text to phone via smtp.')
     parser.add_argument("--reminder",
                         action='store_true',
                         help="Add this flag to not rotate chores, just send reminder of chore for the week")
@@ -94,4 +112,4 @@ if __name__ == "__main__":
 
     fname = "choreFile.txt"
 
-    send_chores(fname, args.reminder)
+    asyncio.run(send_chores(fname, args.reminder))
